3e0aa68bf0c98f0185ed3768f2f8fccae6a9c4ac
Rimi <59701116+AklimaRimi@users.noreply.github.com>
2020-03-02 20:58:42 +0600

    Create 1294.cpp

---
+++ 1294.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   unsigned long long int i,j,m,k,x,n,y,t,z,d,a,ba,bb,bc,bd;
+   char s[123],c[123];
+   map<string,int>mp;
+   map<string,int> ::iterator ip;
+   cin>>t;
+      for(i=1;i<=t;i++)
+      {
+          cin>>n>>m;
+          k = (n*m)/2;
+          cout<<"Case "<<i<<": "<<k<<endl;
+      }
+}
